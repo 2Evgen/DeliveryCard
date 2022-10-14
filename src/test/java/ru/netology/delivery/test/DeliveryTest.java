9904fc2032089281e2db4c2d50651036de2def14
Evgeniya <evgeniya-e92@mail.ru>
2022-10-14 17:15:11 +0400

    change dependencies commit

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,18 +1,20 @@
 package ru.netology.delivery.test;
 
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
+
+    SelenideElement success = $x("//div[@data-test-id='success-notification']");
+    SelenideElement replan = $x("//div[@data-test-id='replan-notification']");
 
     @BeforeEach
     void setup() {
@@ -34,13 +36,14 @@ class DeliveryTest {
         $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $(".button_view_extra").click();
-        $("[data-test-id='notification__title']").shouldBe(visible, Duration.ofSeconds(5)).shouldHave(exactText("Успешно!"));
-        $("[data-test-id='notification__content']").shouldBe(visible, Duration.ofSeconds(5)).shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " + firstMeetingDate));
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(secondMeetingDate);
         $(".button_view_extra").click();
-        $("[data-test-id='replan-notification']").shouldHave(exactText("Необходимо подтверждение У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        replan.$x(".//div[@class='notification__content']").shouldHave(exactText("Необходимо подтверждение У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $("[data-test-id='replan-notification'] .button").click();
-        $("[data-test-id='success-notification']").shouldHave(exactText("Успешно! Встреча успешно запланирована на " + secondMeetingDate));
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " + secondMeetingDate));
     }
+
 }
+
